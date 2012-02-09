4dcfefc0a5ade67abc090bf257184af5e3eb2359
Martin Ueding <dev@martin-ueding.de>
2012-02-09 14:22:10 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
+
+from distutils.core import setup
+
+setup(
+    author = "Martin Ueding",
+    author_email = "<dev@martin-ueding.de>",
+    description = "Parses annotated tags in a git repository.",
+    license = "GPL3",
+    name = "gitchangelog",
+    py_modules = ["gitchangelog"],
+    scripts = ["git-changelog"],
+    version = "1.5.2",
+)
