91bf4b4181948e865af096598e1669fb90d66edc
Martin Ueding <dev@martin-ueding.de>
2012-06-16 14:36:56 +0200

    Include GPLv2+ header

--- setup.py
+++ setup.py
@@ -3,13 +3,29 @@
 
 # Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
 
+###############################################################################
+#                                   License                                   #
+###############################################################################
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
 from distutils.core import setup
 
 setup(
     author = "Martin Ueding",
     author_email = "<dev@martin-ueding.de>",
     description = "Parses annotated tags in a git repository.",
-    license = "GPL3",
+    license = "GPL2+",
     name = "gitchangelog",
     py_modules = ["gitchangelog"],
     scripts = ["git-changelog"],
